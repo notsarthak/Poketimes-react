e3512934fc828b2df30821f17c227a1be158bc43
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-06 20:10:14 +0530

    Created components for multi page app

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About=()=>{
+    return(
+        <div className="container">
+            <h1 className="center">About</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sequi beatae inventore ducimus exercitationem, doloremque quos itaque ipsum dolor voluptates sunt nesciunt quam deserunt minus animi voluptas. Inventore, non assumenda.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home=()=>{
+    return(
+        <div className="container">
+            <h1 className="center">Home</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sequi beatae inventore ducimus exercitationem, doloremque quos itaque ipsum dolor voluptates sunt nesciunt quam deserunt minus animi voluptas. Inventore, non assumenda.</p>
+        </div>
+    )
+}
+
+export default Home;
