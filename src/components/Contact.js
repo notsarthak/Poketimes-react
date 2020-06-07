dea92c61d902cf036912ea92cea61ab07877c333
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-07 18:18:27 +0530

    Programmatic redirect and higher order components intro

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const Contact=()=>{
+const Contact=(props)=>{
+    //console.log(props);
+    /*setTimeout(()=>{
+        props.history.push('/about');
+    },2000);*/
     return(
         <div className="container">
             <h1 className="center">Contact</h1>
             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sequi beatae inventore ducimus exercitationem, doloremque quos itaque ipsum dolor voluptates sunt nesciunt quam deserunt minus animi voluptas. Inventore, non assumenda.</p>
         </div>
-    )
+    );
 }
 
 export default Contact;
