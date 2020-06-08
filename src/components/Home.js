c26889742f6480e73326a0ecc3c842a903753a9a
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-08 15:04:17 +0530

    Fetching data from an API using axios and rendering it to DOM

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react';
+import axios from 'axios';
 
-const Home=()=>{
-    return(
-        <div className="container">
-            <h1 className="center">Home</h1>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sequi beatae inventore ducimus exercitationem, doloremque quos itaque ipsum dolor voluptates sunt nesciunt quam deserunt minus animi voluptas. Inventore, non assumenda.</p>
-        </div>
-    )
+class Home extends Component{
+    state={
+        posts:[]
+    }
+    async componentDidMount(){
+       let res=await axios.get('https://jsonplaceholder.typicode.com/posts');
+       console.log(res)
+       this.setState({
+           posts:res.data.slice(0,10)
+       })
+    }
+    render(){
+        const {posts}=this.state;
+        const postList=posts.length ? (
+            posts.map((post)=>{
+                return(
+                <div className="post card" key={post.id}>
+                    <div className="card-content">
+                        <span className="card-title">{post.title}</span>
+                        <p>{post.body}</p>
+                    </div>
+                </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts yet!</div>
+        );
+        return(
+            <div className="container">
+                <h1 className="center">Home</h1>
+                {postList}
+            </div>
+        )
+    }
 }
 
 export default Home;
