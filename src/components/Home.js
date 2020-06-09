35a8bc4b08beee0e938d658e90b83eb8db46663c
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-09 18:12:36 +0530

    Using images in a component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import Pokeball from '../pokeball.png'
 
 class Home extends Component{
     state={
@@ -19,9 +20,12 @@ class Home extends Component{
             posts.map((post)=>{
                 return(
                 <div className="post card" key={post.id}>
+                    <img src={Pokeball} alt="A Pokeball"/>
                     <div className="card-content">
-                        <span className="card-title"><Link to={`/${post.id}`}>{post.title}</Link></span>
-                        <p>{post.body}</p>
+                        <Link to={`/${post.id}`}>
+                        <span className="card-title">{post.title}</span>
+                        </Link>
+                        <p>{post.body}</p> 
                     </div>
                 </div>
                 )
@@ -30,7 +34,7 @@ class Home extends Component{
             <div className="center">No posts yet!</div>
         );
         return(
-            <div className="container">
+            <div className="container home">
                 <h1 className="center">Home</h1>
                 {postList}
             </div>
